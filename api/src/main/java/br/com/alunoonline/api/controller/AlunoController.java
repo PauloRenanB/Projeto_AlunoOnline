144e95590771cb1b5817b68a6209255b4a1dc4a1
Paulo Renan <127070240+PauloRenanB@users.noreply.github.com>
2024-03-19 12:14:26 -0300

    Criação do read e update de aluno

--- api/src/main/java/br/com/alunoonline/api/controller/AlunoController.java
+++ api/src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -1,12 +1,14 @@
 package br.com.alunoonline.api.controller;
 
 
-
 import br.com.alunoonline.api.model.Aluno;
 import br.com.alunoonline.api.service.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/aluno")
@@ -19,5 +21,23 @@ public class AlunoController {
     @ResponseStatus(HttpStatus.CREATED)
     public void create(@RequestBody Aluno aluno){
         alunoService.create(aluno);
+    }
+
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Aluno> findAll(){
+        return alunoService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Aluno> findById(@PathVariable Long id){
+        return alunoService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody Aluno aluno, @PathVariable Long id){
+        alunoService.update(id, aluno);
     }
 }
